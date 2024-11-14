dc3478f4a88a8d635217671454b97f6c4ede87ef
Pizzox <filippopizzo03@gmail.com>
2024-11-14 23:43:23 +0100

    transaction_product change correctly the stock_level of stock and the total_sold of the product. Need to implement the fact that quantity sold it's only for transaction_type == 'Buy'

--- ingestion.py
+++ ingestion.py
@@ -8,7 +8,7 @@ sales_data = pd.read_csv('data/sample_sales_data.csv')
 
 # Data Transormation: 
 
-sales_data['total_price'] = sales_data['quantity_sold'] * sales_data['unit_price']
+sales_data['total_price'] = sales_data['quantity'] * sales_data['unit_price']
 sales_data['total_sold'] = 0
 sales_data['stock_level'] = 0
 
@@ -63,20 +63,32 @@ for index, row in stocks.iterrows():
 stocks = pd.DataFrame(stocks_toKeep)
 
 #NOW THAT WE HAVE ALL TABLES WITH UNIQUE ROW WE CAN IMPLEMENT THE TABLE TRANSACTION_PRODUCTS THAT IS BASICALLY EVERY CORRECT ROW OF THE CSV FILE AND MODIFY THE VALUES OF THE OTHER DATAFRAME
-transaction_products = sales_data[['transaction_id', 'product_id', 'quantity_sold', 'discount', 'total_price']]
+transaction_products = sales_data[['transaction_id', 'product_id', 'quantity', 'discount', 'total_price']]
 tp_toKeep = []
 for index, row in transaction_products.iterrows():
     if row['product_id'] in unique_products and row['transaction_id'] in unique_transactions:
         tp_toKeep.append(row)
         # refresh the number of products sold
         product_row = products[products['product_id'] == row['product_id']]
-        quantity_sold = product_row['total_sold'].values[0] + row['quantity_sold']
-        products.loc[products['product_id'] == row['product_id'], 'total_sold'] = quantity_sold
+        quantity = product_row['total_sold'].values[0] + row['quantity']
+        products.loc[products['product_id'] == row['product_id'], 'total_sold'] = quantity
         # refresh the number of products in the stock
-        store_id = transactions[transactions['transaction_id'] == row['transaction_id']]['store_id'].values[0]
-        stock_row = stocks[stocks['store_id'] == store_id and stocks['product_id'] == row['product_id']]
-        # if transaction is Sell we substract to the stock, if the transaction is Buy, we add
-        #stock_level = stock_row['stock_level'].values[0] + row[]
+        transaction_row = transactions[transactions['transaction_id'] == row['transaction_id']] #transaction row with the right id
+        store_id = transaction_row['store_id'].values[0] #store id of the transaction
+        stock_row = stocks[(stocks['store_id'] == store_id) & (stocks['product_id'] == row['product_id'])] #stock row with the right id
+
+        # if transaction is Sell we substract to the stock, if the transaction is Buy, we add, otherwise we do nothing
+        #print(f"the stock level is {stock_row['stock_level'].values[0]}")
+        if stock_row.empty:
+            stock_level = row['quantity']
+        elif(transaction_row['transaction_type'].values[0] == 'Buy'):
+            stock_level = stock_row['stock_level'].values[0] + row['quantity']
+        elif(transaction_row['transaction_type'].values[0] == 'Sell'):
+            stock_level = stock_row['stock_level'].values[0] - row['quantity']
+        else:
+            stock_level = stock_row['stock_level'].values[0]
+        stocks.loc[(stocks['product_id']==row['product_id']) & (stocks['store_id']==store_id), 'stock_level'] = stock_level
+
 
 print(products)
 print(transactions)
